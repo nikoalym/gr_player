5d8949b91c8e74b1f55aa597f63913c4d03bc7b2
Nick Alymaras <nick.alymaras@gmail.com>
2025-08-08 18:31:37 +0300

    feat: Update metadata for RootLayout and enhance StreamStats component UI

--- src/components/stream-stats.tsx
+++ src/components/stream-stats.tsx
@@ -14,43 +14,41 @@ export default function StreamStats(props: {
   const unknownCount = streams.length - enabledCount - disabledCount;
   return (
     <div className="mb-2 p-3 bg-gray-700 rounded-lg text-sm md:flex md:flex-col flex gap-3 items-center">
+
       <div className="flex md:flex-col items-center gap-2">
-        <span className="font-normal ">Stream Status</span>
+        <span className="font-normal ">Stream Status:</span>
         <span className="text-xs text-gray-400 ">
           <span className="font-extrabold">{lastUpdated}</span>
         </span>
       </div>
 
-      <div className="flex md:flex-col gap-4">
-        {nowPlaying && (
-          <div className="flex items-center gap-2">
-            <span className="font-medium">Now Playing:</span>{" "}
-            <span className=" font-bold">{nowPlaying.name}</span>
-          </div>
-        )}
+      {nowPlaying && (
+        <div className="flex items-center md:flex-col gap-2">
+          <span className="font-medium">Now Playing:</span>{" "}
+          <span className="font-bold flex-1 w-full">{nowPlaying.name}</span>
+        </div>
+      )}
 
-        {(enabledCount > 0 || disabledCount > 0) && (
-          <div className="bg-gray-700 rounded-lg text-sm ">
-            <div className="flex items-center gap-2">
-              {enabledCount > 0 && (
-                <span>
-                  Working: <span className="font-bold">{enabledCount}</span>
-                </span>
-              )}
-              {disabledCount > 0 && (
-                <span>
-                  Broken: <span className="font-bold">{disabledCount}</span>
-                </span>
-              )}
-              {unknownCount > 0 && (
-                <span>
-                  Untested: <span className="font-bold">{unknownCount}</span>
-                </span>
-              )}
-            </div>
-          </div>
-        )}
-      </div>
+      {(enabledCount > 0 || disabledCount > 0) && (
+        <div className="bg-gray-700 rounded-lg text-sm flex md:flex-col gap-2 items-center">
+          {enabledCount > 0 && (
+            <span>
+              Working: <span className="font-bold">{enabledCount}</span>
+            </span>
+          )}
+          {disabledCount > 0 && (
+            <span>
+              Broken: <span className="font-bold">{disabledCount}</span>
+            </span>
+          )}
+          {unknownCount > 0 && (
+            <span>
+              Untested: <span className="font-bold">{unknownCount}</span>
+            </span>
+          )}
+        </div>
+      )}
+
     </div>
   );
 }
